044d64855fbaa732901714cec091998944a14ec0
nijh <1059186249@qq.com>
2026-04-01 12:26:41 +0800

    feat:dtoLong类型精度缺失bug修复

--- compus-ai-service-llm/src/main/java/com/nijiahao/llm/api/dto/req/AiSessionDto.java
+++ compus-ai-service-llm/src/main/java/com/nijiahao/llm/api/dto/req/AiSessionDto.java
@@ -1,8 +1,12 @@
 package com.nijiahao.llm.api.dto.req;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.Data;
 
 @Data
 public class AiSessionDto {
+
+    @JsonSerialize(using = ToStringSerializer.class) // 必加：防止前端Long类型精度丢失
     private Long robotId;
 }
